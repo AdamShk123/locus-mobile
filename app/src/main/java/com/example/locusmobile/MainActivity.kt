b9686225b9920748171feae575314a6c072031a2
AdamShk123 <adamshkolnik5@gmail.com>
2024-09-20 22:20:17 -0500

    feat: started working on top nav bar for login screen

--- app/src/main/java/com/example/locusmobile/MainActivity.kt
+++ app/src/main/java/com/example/locusmobile/MainActivity.kt
@@ -4,44 +4,88 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.locusmobile.ui.theme.LocusMobileTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LocusMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LocusMobile()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun LocusMobile(modifier: Modifier = Modifier) {
+    LocusMobileTheme {
+        Scaffold(
+            modifier = modifier.fillMaxSize(),
+            topBar = { TopNavigationBar() }
+        ) { innerPadding ->
+            Text(
+                text = "lol",
+                modifier = modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopNavigationBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults
+            .topAppBarColors()
+            .copy(containerColor = colorResource(R.color.red)),
+        title = { Text(text = stringResource(R.string.login_screen_title)) },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Filled.Menu, contentDescription = "Action Icon")
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(Icons.Filled.Person, contentDescription = "Action Icon")
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true
+)
 @Composable
-fun GreetingPreview() {
-    LocusMobileTheme {
-        Greeting("Android")
-    }
+fun LocusMobilePreview() {
+    LocusMobile()
 }
